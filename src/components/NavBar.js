6274f3cdccfb21d338bf84b371e72a3fc5ae1cb8
Yuriko Takamiya <yurikotakamiya@yurikonombp-2.mynetworksettings.com>
2022-03-21 15:38:38 -0400

    fixed some css of nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,49 +5,54 @@ import { Link } from 'react-router-dom';
 const NavBar = () => {
     return (
         <div className="header">
-            <div className="logo">
-                <img 
-                src={img} 
-                width="100" height="100"
-                alt="logo of anywhere fitness"
-                />
+            <div className="header-left">
+                <div className="logo">
+                    <img 
+                    src={img} 
+                    width="80" height="80"
+                    alt="logo of anywhere fitness"
+                    />
+                </div>
+                <div className="name">
+                    <h1>ANYWHERE FITNESS</h1>
+                </div>
             </div>
-            <div>
-                <h1>ANYWHERE FITNESS</h1>
+            <div className="header-right">
+                <nav className="navigation">
+                    <Link to='login'>
+                        <h2>Login</h2>
+                    </Link>
+
+                    <Link to='instructorLogin'>
+                        <h2>Instructor Login</h2>
+                    </Link>
+
+                    <Link to='signup'>
+                        <h2>Sign Up</h2>
+                    </Link>
+
+                    <Link to='client'>
+                        <h2>User Home</h2>
+                    </Link>
+
+                    <Link to='createClass'>
+                        <h2>Create Class</h2>
+                    </Link>
+                    
+                    <Link to='client'>
+                        <h2>Search Class</h2>
+                    </Link>
+
+                    <Link to='instructor'>
+                        <h2>Instructor Home</h2>
+                    </Link>
+
+                    <Link to='logout'>
+                        <h2>Logout</h2>
+                    </Link>
+
+                </nav>
             </div>
-            <nav className="navigation">
-                <Link to='/'>
-                    <h2>Home</h2>
-                </Link>
-                <Link to='login'>
-                    <h2>login</h2>
-                </Link>
-
-                <Link to='instructorLogin'>
-                    <h2>instructorLogin</h2>
-                </Link>
-
-                <Link to='signup'>
-                    <h2>signup</h2>
-                </Link>
-
-                <Link to='client'>
-                    <h2>client</h2>
-                </Link>
-
-                <Link to='createClass'>
-                    <h2>createClass</h2>
-                </Link>
-
-                <Link to='instructor'>
-                    <h2>instructor</h2>
-                </Link>
-
-                <Link to='logout'>
-                    <h2>logout</h2>
-                </Link>
-
-            </nav>
         </div>
     )
 }
