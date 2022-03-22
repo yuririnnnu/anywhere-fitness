0cfd66527d4becaa305d3996ab7655606cb0c714
Yuriko Takamiya <yurikotakamiya@yurikonombp-2.mynetworksettings.com>
2022-03-21 21:13:33 -0400

    add carousel photo to the header element

--- src/components/LoginHeader.js
+++ src/components/LoginHeader.js
@@ -1,16 +1,43 @@
-import img1 from "./../images/fitness3.jpg"; 
+import React from 'react'
+import workout from './../images/workout.jpeg'
+import workout2 from './../images/workout2.jpeg'
+import yoga2 from './../images/yoga2.jpeg'
 
+import { Slide } from 'react-slideshow-image'
+import 'react-slideshow-image/dist/styles.css'
+
+const slideImages = [
+    {
+        url: workout,
+        caption: 'Weight Lifting'
+    },
+    {
+        url: workout2,
+        caption: 'Cardio'
+    },
+    {
+        url: yoga2,
+        caption: 'Yoga'
+    },
+];
 const LoginHeader = () => {
+   
     return (
-        <div>
-            <div className="image">  
-                <img src={img1} width="1200" height="350" alt="fit chick"/>
+        <div className="slide">
+            <div className="slide-container">
+                <Slide>
+                {slideImages.map((slideImage, index)=> (
+                    <div className="each-slide" key={index}>
+                        <div className="each-photo" >
+                            <img src={slideImage.url} />
+                            <span>{slideImage.caption}</span>
+                        </div>
+                    </div>
+                ))} 
+                </Slide>
             </div>
-            <div className="title">
-                <h1>Welcome To AnyWhere Fitness! <br/> Login below to schedule a class!</h1>
-            </div> 
         </div>
     )
 }
-
+    
 export default LoginHeader
