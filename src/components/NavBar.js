818a96202f24926084dd7cb0853fa0025ce75dc9
Yuriko Takamiya <yurikotakamiya@yurikonombp-2.mynetworksettings.com>
2022-03-22 14:21:41 -0400

    implemented humberger menu

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,31 @@
 import React from 'react'; 
 import img from '../images/logo.png';
 import { Link } from 'react-router-dom';
-import useCollapse from 'react-collapsed'
+import { Menus } from './Menus'
+import { slide as Menu } from 'react-burger-menu'
 
 const NavBar = () => {
-    const { getCollapseProps, getToggleProps, isExpanded } = useCollapse();
+    
     return (
         <div className="header">
             <div className="header-left">
+                <Menu>
+                    { Menus.map((menu, index) => {
+                        return (
+                            <li key={index} className={menu.cName}>
+                                <Link to={menu.path}>
+                                    {menu.icon}
+                                    <span>{menu.title}</span>
+                                </Link>
+                            </li>
+                        )
+                    })}            
+                </Menu>    
+            </div>
+            <div className="header-right">
+                <div className="name">
+                    <h1>ANYWHERE FITNESS</h1>
+                </div>
                 <div className="logo">
                     <img 
                     src={img} 
@@ -15,53 +33,9 @@ const NavBar = () => {
                     alt="logo of anywhere fitness"
                     />
                 </div>
-                <div className="name">
-                    <h1>ANYWHERE FITNESS</h1>
-                </div>
-            </div>
-
-
-            <div className="header-right">
-                <nav>                                   
-                    <div className="menu" {...getToggleProps()}>
-                        <h2>{isExpanded ? 'x' : 'Menu '}</h2>
-                    </div>
-                    <div {...getCollapseProps()}>
-                        <div className="navigation">
-                            <h2>- Login</h2>                                
-                            <Link to='login'>
-                                <h2>Member</h2>
-                            </Link>
-                            <Link to='instructorLogin'>
-                                <h2>Instructor</h2>
-                            </Link>                                                                
-                            <Link to='signup'>
-                                <h2>or Sign Up?</h2>
-                            </Link>
-                            <h2>- Home</h2>
-                            <Link to='client'>
-                                <h2>Member</h2>
-                            </Link>
-                            <Link to='instructor'>
-                                <h2>Instructor</h2>
-                            </Link>
-                            <h2>- About Classes</h2>
-                            <Link to='createClass'>
-                                <h2>Create Class</h2>
-                            </Link>                                
-                            <Link to='client'>
-                                <h2>Search Class</h2>
-                            </Link>
-
-                            <Link to='logout'>
-                                <h2>Logout</h2>
-                            </Link>
-                        </div>
-                    </div>                                
-                </nav>
             </div>
         </div>
     )
 }
 
-export default NavBar; 
+export default NavBar
